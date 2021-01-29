778132f9d29d55661cfd239c89be2660f7403cd2
kssadomtsev <sadomcev.k@gmail.com>
2021-01-29 09:47:11 +0300

    Start Security -2

--- src/main/java/ru/onyxone/models/Role.java
+++ src/main/java/ru/onyxone/models/Role.java
@@ -43,4 +43,13 @@ public class Role {
     public void setUsers(Set<User> users) {
         this.users = users;
     }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
 }
--- src/main/java/ru/onyxone/models/User.java
+++ src/main/java/ru/onyxone/models/User.java
@@ -23,7 +23,7 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
